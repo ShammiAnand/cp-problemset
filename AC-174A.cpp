41ba759f523e7e371eec63c0344b248410561874
Shammi Anand <shammianand101@gmail.com>
2020-09-30 18:10:43 +0530

    new

--- AC-174A.cpp
+++ AC-174A.cpp
@@ -1,29 +1,39 @@
 //This code is written by Shammi Anand
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define ll              long long
-#define Max(a,b,c)      max(a,max(b,c))
-#define Min(a,b,c)      min(a,min(b,c))
-#define pb              push_back
-#define mp              make_pair
-#define f               first
-#define s               second
-#define mod             1000000007
-#define nl              "\n"
-#define w(x)            int x; cin>>x; while(x--)
+#define ll long long
+#define Max(a, b, c) max(a, max(b, c))
+#define Min(a, b, c) min(a, min(b, c))
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define mod 1000000007
+#define nl "\n"
+#define w(x)  \
+	int x;    \
+	cin >> x; \
+	while (x--)
 
-void shammi() {
-	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void shammi()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
 }
-int main() {
+int main()
+{
 	// shammi();
-	int X; cin >> X;
-	if (X >= 30) cout << "Yes\n";
-	else cout << "No\n";
+	int X;
+	cin >> X;
+	if (X >= 30)
+		cout << "Yes\n";
+	else
+		cout << "No\n";
 	return 0;
 }
---
+++ CF-1420A.cpp
@@ -0,0 +1,59 @@
+/*
+This code is written by Shammi Anand
+contact : shammianand101@gmail.com, shammianand.me
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll              long long
+#define pb              push_back
+#define f               first
+#define s               second
+#define vi              vector<ll>
+#define pii             pair<int, int>
+#define rep(i,a,n)      for(int i=a;i<n;i++)
+#define F(i,n)          for(int i=0;i<n;i++)
+#define all(a)          a.begin(), a.end()
+#define INF             1e9+7
+#define nl              "\n"
+#define w(x)            int x; cin>>x; while(x--)
+
+template<typename T_vector>
+void output(const T_vector &v, int start = -1, int end = -1) {
+	if (start < 0) start = 0;
+	if (end < 0) end = int(v.size());
+
+	for (int i = start; i < end; i++) {
+		cout << v[i] << (i < end - 1 ? ' ' : '\n');
+	}
+}
+
+void shammi() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+
+int main() {
+	shammi();
+	w(t) {
+		ll n; cin >> n;
+		vi a(n);
+		for (ll& elem : a) cin >> elem;
+
+		bool can = false;
+		for (int i = 1; i < n; i++) {
+			if (a[i] >= a[i - 1]) {
+				can = true;
+				break;
+			}
+		}
+		if (can) cout << "YES" << '\n';
+		else cout << "NO" << '\n';
+	}
+	return 0;
+}
